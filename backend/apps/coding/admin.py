211c1d2c6992838409053af2dc2a42d024d28529
JMakoti <josephmwamuye11@gmail.com>
2025-09-09 00:20:02 +0300

    Deleted backend project folder

--- backend/apps/coding/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import CodingSession
-
-# Register your models here.
-admin.site.register(CodingSession)
-class CodingSessionAdmin(admin.ModelAdmin):
-    readonly_fields = ('credits_awarded')
